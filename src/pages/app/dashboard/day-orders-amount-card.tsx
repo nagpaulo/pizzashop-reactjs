ed96134c6250896d7dad3e6e756a64e9de7f9fff
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2025-02-16 15:31:03 -0300

    ajustando span

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -15,7 +15,7 @@ export function DayOrdersAmountCard(props: DayOrdersAmountCardProps) {
         <CardContent className="space-y-1">
             <span className="text-2xl font-bold tracking-tight">12</span>
             <p className="text-xs text-muted-foreground">
-            <span className="text-rose-500 dark:text-rose-400">-4%</span> em relação ao dia anterior.
+              <span className="text-rose-500 dark:text-rose-400">-4%</span> em relação ao dia anterior.
             </p>
         </CardContent>
     </Card>
